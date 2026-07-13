483947a415ff85b66232f84bbcb2597f7d986d16
Ashwin I S <i.s.ashwin2006@gmail.com>
2026-07-13 21:31:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.24%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        digits = "123456789"
+        res = []
+        
+        for length in range(len(str(low)), len(str(high)) + 1):
+            for start in range(0, 10 - length):
+                num = int(digits[start:start + length])
+                if low <= num <= high:
+                    res.append(num)
+        
+        return res
